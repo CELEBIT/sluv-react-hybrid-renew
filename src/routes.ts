acc9fbc77c22ddb914d68729497b43402f44bd5f
Boin Kim <charles1236@kau.kr>
2023-03-11 19:09:48 +0900

    [#7] feat : 하단바 관련 프로젝트 페이지 설계
    - 홈 : '/'
    - 커뮤니티 : '/community'
    - 아이템 정보 업로드 폼 : '/item/create'
    - 옷장 : '/closet'
    - MY : '/my'
    
    - MY의 경우, path variable로 처리하고 싶지만, 아직 어떻게 처리할지 고민 중

--- src/routes.ts
+++ src/routes.ts
@@ -7,10 +7,17 @@ const Page500 = React.lazy(() => import('./pages/page500'));
 // 하단바 네비게이션 페이지
 const Home = React.lazy(() => import('./pages/home'));
 const Community = React.lazy(() => import('./pages/community'));
+const ItemCreate = React.lazy(() => import('./pages/item/create'));
+const Closet = React.lazy(() => import('./pages/closet'));
+const User = React.lazy(() => import('./pages/user'))
+
 
 const routes = [
   { path: '/', element: Home },
   { path: '/community', element: Community },
+  { path: '/item/create', element: ItemCreate },
+  { path: '/closet', element: Closet },
+  { path: '/user', element: User },
   { path: '/404', element: Page404 },
   { path: '/500', element: Page500 },
 ]
