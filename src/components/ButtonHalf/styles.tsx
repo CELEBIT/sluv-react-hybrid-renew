89db13f81da7cb2f22dc055e205d4783174d4e36
ezenjun <ezenjun07@gmail.com>
2023-04-10 19:39:36 +0900

    [#23] refactor : Half Button 위치별 사이즈 변경

--- src/components/ButtonHalf/styles.tsx
+++ src/components/ButtonHalf/styles.tsx
@@ -3,6 +3,7 @@ import { Common } from '../styles'
 
 export const HalfWrapper = styled.div<{
   type: string
+  isbottom?: boolean
 }>`
   display: inline-flex;
   flex-direction: row;
@@ -10,18 +11,28 @@ export const HalfWrapper = styled.div<{
   justify-content: center;
   align-items: center;
 
-  height: 3.5rem;
+  height: 3rem;
   width: 7.5rem;
-  padding: 1.0938rem 0.9rem;
+  padding: 0.9375rem;
   border-radius: 0.5rem;
 
   font-family: 'Pretendard';
   font-weight: 500;
-  font-size: 1.125rem;
+  font-size: 0.9375rem;
 
   color: ${(props) => (props.type === 'confirm' ? 'white' : Common.colors.BK)};
   background-color: ${(props) =>
     props.type === 'confirm' ? Common.colors.COMPLETE : Common.colors.GR200};
+
+  ${({ isbottom }) =>
+    isbottom === true &&
+    `
+    height: 3.5rem;
+    width: 10.1875rem;
+    padding: 1.0625rem;
+    border-radius: 0.5rem;
+    font-size: 1.125rem;
+    `}
 
   p {
     margin: 0;
