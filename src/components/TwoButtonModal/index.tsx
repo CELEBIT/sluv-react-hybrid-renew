260368c250797e8c4b08cf02a8e7aadfb71765fb
Boin Kim <charles1236@kau.kr>
2023-04-10 16:15:04 +0900

    [#13] feat : AskRecentPostWritingModal 생성

---
+++ src/components/TwoButtonModal/index.tsx
@@ -0,0 +1,31 @@
+import { BtnModalContainer } from '../Modals/styles';
+import React from 'react';
+import ButtonHalf from '../ButtonHalf/ButtonHalf';
+
+interface TwoButtonModalProps {
+  leftButtonName: string
+  rightButtonName: string
+  leftButtonOnClick: <T>(args: T) => void
+  rightButtonOnClick: <T>(args: T) => void
+  children: React.ReactNode
+}
+
+const TwoButtonModal = ({
+  leftButtonName,
+  rightButtonName,
+  leftButtonOnClick,
+  rightButtonOnClick,
+  children,
+}: TwoButtonModalProps) => {
+  return (
+    <BtnModalContainer>
+      {children}
+      <div style={{display: 'flex'}}> 
+        <ButtonHalf type='cancel' text={leftButtonName} onClick={() => leftButtonOnClick} />
+        <ButtonHalf type='confimr' text={rightButtonName} onClick={() => rightButtonOnClick} />
+      </div>
+    </BtnModalContainer>
+  )
+}
+
+export default TwoButtonModal;
