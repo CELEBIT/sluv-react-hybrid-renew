d0d60390994a0eb6ed36c768f3f3b1975a02c5b6
ezenjun <ezenjun07@gmail.com>
2023-04-11 19:22:50 +0900

    [#30] feat : TextArea Component 생성

---
+++ src/components/TextField/TextArea/TextArea.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import { TextAreaWrapper, Textarea } from './styles'
+import { ReactComponent as Delete } from '../../../assets/delete_textfield_24.svg'
+import { ErrorText } from '../DefaultTextfield/styles'
+interface TextAreaProps {
+  value: string
+  setValue: React.Dispatch<React.SetStateAction<string>>
+  placeholder: string
+  error: boolean
+  errorMsg: string
+}
+
+const TextArea = ({ value, setValue, placeholder, error, errorMsg }: TextAreaProps) => {
+  const [isFocused, setIsFocused] = useState(false)
+  const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setValue(event.target.value)
+    console.log('value:', value)
+  }
+  const onDelete = () => {
+    setValue('')
+  }
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <TextAreaWrapper onFocus={() => setIsFocused(true)} onBlur={() => setIsFocused(false)}>
+        <Textarea
+          value={value}
+          placeholder={placeholder}
+          onChange={handleInputChange}
+          maxLength={300}
+        ></Textarea>
+        {value.length !== 0 && isFocused && (
+          <Delete
+            style={{ marginLeft: '0.625rem' }}
+            onClick={onDelete}
+            onMouseDown={onDelete}
+            onTouchStart={onDelete}
+          ></Delete>
+        )}
+      </TextAreaWrapper>
+      {error && <ErrorText>{errorMsg}</ErrorText>}
+    </div>
+  )
+}
+
+export default TextArea
