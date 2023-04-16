db5e14be196dc5977f67938d66f1471bb8aaadcc
ezenjun <ezenjun07@gmail.com>
2023-04-16 16:27:31 +0900

    [#33] feat : Date,Place Textfield 생성
    DatePicker, 장소 입력 모달 제외 순수 Component 디자인만 생성

---
+++ src/pages/item/create/component/DatePlaceField/style.tsx
@@ -0,0 +1,59 @@
+import styled from 'styled-components'
+import { Common } from '../../../../../components/styles'
+
+export const DatePlaceWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  height: 5.375rem;
+  width: 100%;
+  border-radius: 0.5rem;
+  padding: 0.625rem 1.25rem;
+  border: 0.0625rem solid ${Common.colors.GR200};
+  font-family: 'Pretendard';
+`
+
+export const DateWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  min-width: 4.75rem;
+  height: 2.75rem;
+`
+export const PlaceWrapper = styled.div`
+  display: flex;
+  overflow: hidden;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 2.75rem;
+`
+
+export const Line = styled.div`
+  width: 0.0625rem;
+  height: 4.125rem;
+  background: #dfe2e6;
+  margin: 0 1.25rem;
+`
+
+export const Title = styled.span`
+  margin: 0;
+  font-weight: 500;
+  font-size: 0.9375rem;
+  color: ${Common.colors.GR600};
+`
+export const ValueText = styled.span<{
+  isEmpty?: boolean
+}>`
+  margin: 0;
+  font-weight: 400;
+  font-size: 1.0625rem;
+  color: ${Common.colors.BK};
+  ${({ isEmpty }) =>
+    isEmpty &&
+    `
+      color: ${Common.colors.GR500};
+    `}
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`
--- src/pages/item/create/index.tsx
+++ src/pages/item/create/index.tsx
@@ -1,17 +1,19 @@
 import React from 'react'
-import { modals } from '../../../components/Modals';
+import { modals } from '../../../components/Modals'
 import useModals from '../../../components/Modals/hooks/useModals'
 
 const ItemCreate = () => {
-  const { openModal } = useModals();
+  const { openModal } = useModals()
 
   const test = () => {
-    console.log('test');
-    openModal(modals.AskRecentPostWritingModal, {});
+    console.log('test')
+    openModal(modals.AskRecentPostWritingModal, {})
   }
 
   return (
-    <div onClick={test}>ItemCreate</div>
+    <div onClick={test} style={{ padding: '20px' }}>
+      <div>ItemCreate</div>
+    </div>
   )
 }
 
