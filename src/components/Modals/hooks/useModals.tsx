0c5eee8d23b313b6da18923f7d7aecfeaa0f6cd8
ezenjun <ezenjun07@gmail.com>
2023-04-16 16:33:46 +0900

    [#33] style : staged commit

--- src/components/Modals/hooks/useModals.tsx
+++ src/components/Modals/hooks/useModals.tsx
@@ -1,56 +1,51 @@
-import { atom, useRecoilState } from 'recoil';
-import { useCallback, ComponentProps, FunctionComponent, useEffect } from 'react';
+import { atom, useRecoilState } from 'recoil'
+import { useCallback, ComponentProps, FunctionComponent, useEffect } from 'react'
 const modalsAtom = atom<
   Array<{
-    Component: FunctionComponent<any>;
-    props: ComponentProps<FunctionComponent<any>>;
+    Component: FunctionComponent<any>
+    props: ComponentProps<FunctionComponent<any>>
   }>
 >({
   key: 'modalsAtom',
   default: [],
-});
+})
 
 const useModals = () => {
-  const [modals, setModals] = useRecoilState(modalsAtom);
+  const [modals, setModals] = useRecoilState(modalsAtom)
 
   useEffect(() => {
-    const goBack = () => setModals([]);
-    window.addEventListener('popstate', goBack);
+    const goBack = () => setModals([])
+    window.addEventListener('popstate', goBack)
     return () => {
-      window.removeEventListener('popstate', goBack);
-    };
+      window.removeEventListener('popstate', goBack)
+    }
   }, [])
 
   const openModal = useCallback(
     <T extends FunctionComponent<any>>(
       Component: T,
-      props?: Omit<ComponentProps<T>, 'open'>
+      props?: Omit<ComponentProps<T>, 'open'>,
     ): void => {
-      setModals((modals) => [
-        ...modals,
-        { Component, props: { ...props, open: true } },
-      ]);
-      history.pushState({page:'modal'}, document.title);
+      setModals((modals) => [...modals, { Component, props: { ...props, open: true } }])
+      history.pushState({ page: 'modal' }, document.title)
     },
-    [setModals]
-  );
+    [setModals],
+  )
 
   const closeModal = useCallback(
     <T extends FunctionComponent<any>>(Component: T, callbackFunc?: () => void) => {
-      setModals((modals) =>
-        modals.filter((modal) => modal.Component !== Component)
-      );
-      if (callbackFunc) callbackFunc();
-      else history.back();
+      setModals((modals) => modals.filter((modal) => modal.Component !== Component))
+      if (callbackFunc) callbackFunc()
+      else history.back()
     },
-    [setModals]
-  );
+    [setModals],
+  )
 
   return {
     modals,
     openModal,
     closeModal,
-  };
-};
+  }
+}
 
-export default useModals;
+export default useModals
