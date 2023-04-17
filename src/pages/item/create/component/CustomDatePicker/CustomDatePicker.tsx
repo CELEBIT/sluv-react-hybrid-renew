2080af94e36fd0721b060545b217ca23d8b10376
ezenjun <ezenjun07@gmail.com>
2023-04-17 18:28:40 +0900

    [#33] feat : DatePicker 생성

--- src/pages/item/create/component/CustomDatePicker/CustomDatePicker.tsx
+++ src/pages/item/create/component/CustomDatePicker/CustomDatePicker.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
-import { CustomDatePickerView } from './styles'
+import { CustomDatePickerWrapper, CustomDatePickerView } from './styles'
 
-const CustomDatepicker = () => {
+interface DatePickerProps {
+  value: Date
+  setValue: React.Dispatch<React.SetStateAction<Date>>
+}
+
+const CustomDatepicker = ({ value, setValue }: DatePickerProps) => {
   const now = new Date()
-  // const [value, setValue] = useState<Date>(now)
   const onChangeDate = (date: Date) => {
+    setValue(date)
     console.log(date)
   }
   const labelRenderer = (type: string, data: number) => {
@@ -20,13 +25,16 @@ const CustomDatepicker = () => {
     }
   }
   return (
-    <CustomDatePickerView
-      defaultValue={now}
-      max={now}
-      onChange={onChangeDate}
-      renderLabel={labelRenderer}
-      style={{ justifyContent: 'center' }}
-    />
+    <CustomDatePickerWrapper>
+      <CustomDatePickerView
+        defaultValue={now}
+        max={now}
+        value={value}
+        onChange={onChangeDate}
+        renderLabel={labelRenderer}
+        style={{ justifyContent: 'center', width: '80%' }}
+      />
+    </CustomDatePickerWrapper>
   )
 }
 
