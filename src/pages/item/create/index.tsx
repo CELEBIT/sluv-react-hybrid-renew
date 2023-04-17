7d831e4ad572e65bf885099274fe0ca21a4b6494
ezenjun <ezenjun07@gmail.com>
2023-04-17 16:55:40 +0900

    [#33] feat : DatePicker Custom 진행중

--- src/pages/item/create/index.tsx
+++ src/pages/item/create/index.tsx
@@ -1,18 +1,32 @@
 import React from 'react'
-import { modals } from '../../../components/Modals'
-import useModals from '../../../components/Modals/hooks/useModals'
+// import { modals } from '../../../components/Modals'
+// import useModals from '../../../components/Modals/hooks/useModals'
+import CustomDatePicker from './component/CustomDatePicker/CustomDatePicker'
 
 const ItemCreate = () => {
-  const { openModal } = useModals()
+  // const { openModal } = useModals()
 
-  const test = () => {
-    console.log('test')
-    openModal(modals.AskRecentPostWritingModal, {})
-  }
+  // const test = () => {
+  //   console.log('test')
+  //   openModal(modals.AskRecentPostWritingModal, {})
+  // }
 
+  // const [selectedDate, setSelectedDate] = useState<Date>(new Date())
+
+  // const handleDateChange = (date: Date) => {
+  //   setSelectedDate(date)
+  // }
   return (
-    <div onClick={test} style={{ padding: '20px' }}>
-      <div>ItemCreate</div>
+    <div
+      style={{
+        display: 'flex',
+        width: '18.75rem',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: '1.25rem',
+      }}
+    >
+      <CustomDatePicker />
     </div>
   )
 }
