ffd59a23e9ad320c64ddafb6aa3c039bff794103
ezenjun <ezenjun07@gmail.com>
2023-04-18 22:16:40 +0900

    [#33] style : create index.tsx 비움

--- src/pages/item/create/index.tsx
+++ src/pages/item/create/index.tsx
@@ -1,13 +1,7 @@
 import React from 'react'
-import DatePlaceField from './component/DatePlaceField/DatePlaceField'
 
 const ItemCreate = () => {
-  return (
-    <div>
-      ItemCreate
-      <DatePlaceField></DatePlaceField>
-    </div>
-  )
+  return <div>ItemCreate</div>
 }
 
 export default ItemCreate
