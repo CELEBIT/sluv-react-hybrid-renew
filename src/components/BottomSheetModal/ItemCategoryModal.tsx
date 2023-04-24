9991561b54142dfd1c8e4c9ea4b51625f11b6b90
Boin Kim <charles1236@kau.kr>
2023-04-24 14:40:56 +0900

    [#41] feat: ItemCategoryModal 제작 및 아이템 카테고리 조회 API 연동

---
+++ src/components/BottomSheetModal/ItemCategoryModal.tsx
@@ -0,0 +1,28 @@
+import styled from '@emotion/styled'
+import React from 'react'
+import BottomSheetModal from '.'
+import useItemCategoryQuery from '../../apis/item/hooks/useItemCategoryQuery'
+import Header from '../Header/Header'
+
+const ItemCategoryModal = () => {
+  const {
+    getItemCategory: { data },
+  } = useItemCategoryQuery()
+  console.log(data)
+
+  return (
+    <BottomSheetModal>
+      <ModalWrapper>
+        <Header isModalHeader={true} title={'아이템 종류'} />
+      </ModalWrapper>
+    </BottomSheetModal>
+  )
+}
+
+export default ItemCategoryModal
+
+const ModalWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 0 1.25rem;
+`
--- src/pages/item/create/index.tsx
+++ src/pages/item/create/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { modals } from '../../../components/Modals'
+import useModals from '../../../components/Modals/hooks/useModals'
 
 const ItemCreate = () => {
-  return <div>ItemCreate</div>
+  const { openModal } = useModals()
+
+  return <div onClick={() => openModal(modals.ItemCategoryModal)}>ItemCreate</div>
 }
 
 export default ItemCreate
