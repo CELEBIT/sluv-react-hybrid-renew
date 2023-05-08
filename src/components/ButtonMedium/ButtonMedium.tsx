0fb32516b81943b7b7d55b182a50c8b37ab4667c
ezenjun <ezenjun07@gmail.com>
2023-05-08 17:46:50 +0900

    [#67] feat : 아이템 카테고리 선택 완료

--- src/components/ButtonMedium/ButtonMedium.tsx
+++ src/components/ButtonMedium/ButtonMedium.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, forwardRef } from 'react'
 import { MediumWrapper } from './styles'
 import { ReactComponent as Search } from '../../assets/search_18.svg'
 import { Common } from '../styles'
@@ -7,40 +7,48 @@ interface ButtonMediumProps {
   text: string
   icon?: boolean
   type: string
-  //  type 상태는 pri, sec, disable 로 디자인 구분
   active?: boolean
-  // active 는 클릭 여부
   error?: boolean
-
   onClick: () => void
 }
 
-const ButtonMedium = ({ text, icon, type, active, error, onClick }: ButtonMediumProps) => {
-  const [color, setColor] = useState('')
-  useEffect(() => {
-    if (type == 'sec') {
-      setColor(Common.colors.BK)
-    } else {
-      setColor(Common.colors.GR500)
-    }
-  }, [])
+const ButtonMedium = forwardRef<HTMLDivElement, ButtonMediumProps>(
+  ({ text, icon, type, active, error, onClick }, ref) => {
+    const [color, setColor] = useState('')
+    useEffect(() => {
+      if (type === 'sec') {
+        setColor(Common.colors.BK)
+      } else {
+        setColor(Common.colors.GR500)
+      }
+    }, [])
 
-  return (
-    <MediumWrapper icon={icon} type={type} active={active} error={error} onClick={onClick}>
-      {icon ? (
-        <div>
-          {active ? (
-            <Search width='18' height='18' fill={Common.colors.SEC} />
-          ) : (
-            <Search width='18' height='18' fill={color} />
-          )}
-        </div>
-      ) : (
-        <></>
-      )}
-      <p>{text}</p>
-    </MediumWrapper>
-  )
-}
+    return (
+      <MediumWrapper
+        icon={icon}
+        type={type}
+        active={active}
+        error={error}
+        onClick={onClick}
+        ref={ref}
+      >
+        {icon ? (
+          <div>
+            {active ? (
+              <Search width='18' height='18' fill={Common.colors.SEC} />
+            ) : (
+              <Search width='18' height='18' fill={color} />
+            )}
+          </div>
+        ) : (
+          <></>
+        )}
+        <p>{text}</p>
+      </MediumWrapper>
+    )
+  },
+)
+
+ButtonMedium.displayName = 'ButtonMedium'
 
 export default ButtonMedium
--- src/components/TextField/DefaultTextfield/styles.tsx
+++ src/components/TextField/DefaultTextfield/styles.tsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled from '@emotion/styled'
 import { Common } from '../../styles'
 
 export const InputContainer = styled.div`
