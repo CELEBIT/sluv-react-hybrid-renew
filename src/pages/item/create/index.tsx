028be756d09f26812f3ff0ce2909d470056513c3
ezenjun <ezenjun07@gmail.com>
2023-05-10 20:06:20 +0900

    [#70] feat : 아이템 업로드 페이지 Error 케이스 완료

--- src/pages/item/create/index.tsx
+++ src/pages/item/create/index.tsx
@@ -4,10 +4,13 @@ import BrandItemField, {
   selectedBrandState,
 } from './components/BrandItemField/BrandItemField'
 import { useRecoilValue } from 'recoil'
-import DatePlaceField from './components/DatePlaceField/DatePlaceField'
-import PriceField from './components/PriceField/PriceField'
+import DatePlaceField, {
+  selectedDateState,
+  selectedPlaceState,
+} from './components/DatePlaceField/DatePlaceField'
+import PriceField, { itemPriceState } from './components/PriceField/PriceField'
 import { useNavigate } from 'react-router-dom'
-import SelectCeleb from '../../../components/SelectCeleb/SelectCeleb'
+import SelectCeleb, { selectedCelebState } from '../../../components/SelectCeleb/SelectCeleb'
 import SelectCategory from './components/SelectCategory/SelectCategory'
 import Header from '../../../components/Header/Header'
 import {
@@ -27,6 +30,10 @@ import { ReactComponent as InfoAddOff } from '../../../assets/info_add_off_20.sv
 import { ReactComponent as StorageOff } from '../../../assets/storage_off_20.svg'
 import { HeaderWrapper } from '../addInfo/styles'
 import { ErrorText } from '../../../components/TextField/DefaultTextfield/styles'
+import { selectedSubCategoryState } from '../../../components/BottomSheetModal/ItemCategoryModal'
+import { addInfoTextState } from '../addInfo'
+import { linksState } from '../addLink/components/LinkInput/LinkInput'
+import { infoSourceState } from '../addInfo/components/sourceInput/SourceInput'
 // const Itzy = {
 //   id: 0,
 //   celebNameKr: '있지',
@@ -64,30 +71,49 @@ import { ErrorText } from '../../../components/TextField/DefaultTextfield/styles
 
 const ItemCreate = () => {
   const navigate = useNavigate()
-  // const [celebValid, setCelebValid] = useState(true)
-  const [brandValid, setBrandValid] = useState(true)
-  const [itemValid, setItemValid] = useState(true)
+  const celeb = useRecoilValue(selectedCelebState)
+  const date = useRecoilValue(selectedDateState)
+  const place = useRecoilValue(selectedPlaceState)
+  const category = useRecoilValue(selectedSubCategoryState)
   const brand = useRecoilValue(selectedBrandState)
   const itemName = useRecoilValue(itemNameState)
+  const price = useRecoilValue(itemPriceState)
+  const additionalInfo = useRecoilValue(addInfoTextState)
+  const infoSource = useRecoilValue(infoSourceState)
+  const links = useRecoilValue(linksState)
   const [hasTriedToUpload, setHasTriedToUpload] = useState(false)
 
   const onSubmit = () => {
     setHasTriedToUpload(true)
-    if (!brand) {
-      setBrandValid(false)
-      alert('empty brand')
-      console.log('empty brand')
-    } else {
-      setBrandValid(true)
-    }
-    if (!itemName) {
-      setItemValid(false)
-    } else {
-      setItemValid(true)
-    }
-    if (brand && itemName) {
+    if (celeb.id && category.id && brand.id && itemName && price) {
       alert('success')
+    } else {
+      alert('fail')
     }
+    const newItem = {
+      id: 0,
+      imgList: [
+        {
+          imgUrl: 'string',
+          representFlag: true,
+        },
+      ],
+      celebId: celeb.id,
+      whenDiscovery: date ? date.toISOString() : null,
+      whereDiscovery: place ? place : null,
+      categoryId: category.id,
+      brandId: brand.id,
+      itemName: itemName,
+      price: price !== 0 ? price : null,
+      color: null,
+      additionalInfo: additionalInfo ? additionalInfo : null,
+      hashTagIdList: [0],
+      linkList: links[0].linkName ? links : null,
+      infoSource: infoSource ? infoSource : null,
+      newCelebId: 0,
+      newBrandId: 0,
+    }
+    console.log(newItem)
   }
 
   return (
@@ -103,11 +129,11 @@ const ItemCreate = () => {
       <ComponentContainer>
         <ComponentWrapper>
           <LabelContainer>
-            {brandValid && <Error></Error>}
+            {hasTriedToUpload && !celeb.id && <Error></Error>}
             <Label>누가 착용했나요?</Label>
           </LabelContainer>
           <SelectCeleb></SelectCeleb>
-          {hasTriedToUpload === false && brandValid === true && (
+          {hasTriedToUpload && !celeb.id && (
             <ErrorText className='error'>필수 항목입니다</ErrorText>
           )}
         </ComponentWrapper>
@@ -117,24 +143,21 @@ const ItemCreate = () => {
         </ComponentWrapper>
         <ComponentWrapper>
           <LabelContainer>
-            {brandValid && <Error></Error>}
+            {hasTriedToUpload && (!category.id || !brand || !itemName || !price) && <Error></Error>}
             <Label>어떤 아이템인가요?</Label>
           </LabelContainer>
           <SelectCategory />
-          {hasTriedToUpload === false && brandValid === true && (
+          {hasTriedToUpload && !category.id && (
             <ErrorText className='error'>필수 항목입니다</ErrorText>
           )}
           <BrandItemField
-            brandValid={hasTriedToUpload ? brandValid : true}
-            itemNameValid={hasTriedToUpload ? itemValid : true}
+            brandValid={hasTriedToUpload ? !brand : true}
+            itemNameValid={hasTriedToUpload ? itemName !== '' : true}
           ></BrandItemField>
-          {hasTriedToUpload === false && brandValid === true && (
-            <ErrorText className='error'>필수 항목입니다</ErrorText>
-          )}
           {brand.id && (
             <>
               <PriceField></PriceField>
-              {hasTriedToUpload === false && brandValid === true && (
+              {hasTriedToUpload && !price && (
                 <ErrorText className='error'>필수 항목입니다</ErrorText>
               )}
             </>
@@ -152,7 +175,7 @@ const ItemCreate = () => {
             </div>
           </div>
           <div className='right'>
-            <StorageOff onClick={() => navigate('/item/create/addlink')}></StorageOff>
+            <StorageOff onClick={() => navigate('/item/create/temporary-storage')}></StorageOff>
           </div>
         </BottomBar>
       </ComponentContainer>
