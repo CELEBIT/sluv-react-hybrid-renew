a25635616c12f308464727338f81b08a9d39b386
Boin Kim <charles1236@kau.kr>
2023-05-11 20:40:17 +0900

    [#62] feat : 최근 선택한 브랜드 선택 삭제 API 연동

--- src/apis/brand/hooks/useRecentBrandQuery.tsx
+++ src/apis/brand/hooks/useRecentBrandQuery.tsx
@@ -1,13 +1,29 @@
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import BrandService from '../brandService'
 import { queryKeys } from '../../../config/queryKeys'
+import { BrandFlag } from '../../core/type'
+
+interface IDeleteBrand {
+  brandId: number
+  flag: BrandFlag
+}
 
 const useRecentBrandQuery = () => {
   const brand = new BrandService()
+  const queryClient = useQueryClient()
   const getBrandRecentSelected = useQuery(queryKeys.brandRecentSelected, () =>
     brand.getBrandRecentSelected(),
   )
-  return { getBrandRecentSelected }
+  const deleteRecentBrand = useMutation(
+    ({ brandId, flag }: IDeleteBrand) => brand.deleteRecentBrand(brandId, flag),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(queryKeys.brandRecentSelected)
+      },
+    },
+  )
+
+  return { getBrandRecentSelected, deleteRecentBrand }
 }
 
 export default useRecentBrandQuery
