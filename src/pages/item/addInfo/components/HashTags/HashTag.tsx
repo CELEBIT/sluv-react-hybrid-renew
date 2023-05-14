a3180aee0be18c873bb6085c9bb5d0f1f96afcf7
ezenjun <ezenjun07@gmail.com>
2023-05-14 16:06:19 +0900

    [#79] refactor : HashTag 완성

--- src/pages/item/addInfo/components/HashTags/HashTag.tsx
+++ src/pages/item/addInfo/components/HashTags/HashTag.tsx
@@ -1,37 +1,76 @@
-import React, { useEffect, useRef, useState } from 'react'
-import { HashTagInput } from './styles'
+import React, { useState, useRef, useEffect } from 'react'
+import { SearchedHashTag, Tag, TagInput, TagInputContainer } from './styles'
 import debounce from 'lodash/debounce'
-import { parseHashTags } from './Hashtag.util'
-import { atom, useRecoilValue } from 'recoil'
+import { atom, useRecoilState } from 'recoil'
 import { atomKeys } from '../../../../../config/atomKeys'
+import { HashTagWrapper } from '../../styles'
+import HighlightedText from '../../../../../components/HighlightedText/HighlightedText'
 
 interface HashtagInputProps {
   placeholder: string
   onChange: (hashtags: string[]) => void
 }
 
-export const hashTagState = atom<Array<string>>({
+export const hashTagState = atom<string[]>({
   key: atomKeys.hashTagState,
   default: [],
 })
 
-const HashtagInput: React.FC<HashtagInputProps> = ({ placeholder, onChange }) => {
-  const hashTag = useRecoilValue(hashTagState)
-  const [value, setValue] = useState<string>(hashTag.map((tag) => `#${tag}`).join(' '))
-  const textareaRef = useRef<HTMLTextAreaElement>(null)
-  const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-    const inputValue = event.target.value
-    const hashtags = parseHashTags(inputValue)
-    setValue(inputValue)
-    onChange(hashtags)
+const HashtagInput: React.FC<HashtagInputProps> = ({ placeholder }) => {
+  const [hashTags, setHashtags] = useRecoilState(hashTagState)
 
-    const cursorPosition = event.currentTarget?.selectionStart ?? 0
-    const inputText = event.currentTarget?.value ?? ''
-    const match = inputText.substring(0, cursorPosition).match(/(^|\s)#(\p{L}+)$/u)
+  const [currentTag, setCurrentTag] = useState<string>('')
+  const searchResult = [
+    {
+      hashtagId: 1,
+      hashtagContent: '애착템',
+      count: 4,
+    },
+    {
+      hashtagId: 2,
+      hashtagContent: '애착템으로',
+      count: 144,
+    },
+    {
+      hashtagId: 3,
+      hashtagContent: '애착템할래',
+      count: 6,
+    },
+  ]
+  useEffect(() => {
+    const handleBackspace = (event: KeyboardEvent) => {
+      if (event.key === 'Backspace' && currentTag === '') {
+        setHashtags((prevTags) => prevTags.slice(0, prevTags.length - 1))
+      }
+    }
+    document.addEventListener('keydown', handleBackspace)
+    console.log(hashTags)
+    return () => {
+      document.removeEventListener('keydown', handleBackspace)
+    }
+  }, [currentTag])
 
-    if (match) {
-      const hashtag = match[0].substring(1)
-      debouncedSearch(hashtag)
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCurrentTag(event.target.value)
+    debouncedSearch(event.target.value)
+  }
+
+  const handleInputKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === ' ') {
+      event.preventDefault()
+      if (currentTag.trim() !== '') {
+        setHashtags((prevTags) => [...prevTags, currentTag.trim()])
+        setCurrentTag('')
+      }
+    }
+  }
+  const tagInputRef = useRef<HTMLInputElement>(null)
+  const onClickSearchedHashtag = (hashtagContent: string) => {
+    setCurrentTag(hashtagContent)
+    setHashtags((prevTags) => [...prevTags, hashtagContent.trim()])
+    setCurrentTag('')
+    if (tagInputRef.current) {
+      tagInputRef.current.focus()
     }
   }
 
@@ -41,23 +80,41 @@ const HashtagInput: React.FC<HashtagInputProps> = ({ placeholder, onChange }) =>
       // replace with actual search API call
     }, 300),
   ).current
-
-  useEffect(() => {
-    // textarea scroll height 설정
-    if (textareaRef.current) {
-      textareaRef.current.style.height = '0px'
-      const scrollHeight = textareaRef.current.scrollHeight
-      textareaRef.current.style.height = scrollHeight + 'px'
-    }
-  }, [value])
-
   return (
-    <HashTagInput
-      placeholder={placeholder}
-      value={value}
-      onChange={handleInputChange}
-      ref={textareaRef}
-    />
+    <HashTagWrapper>
+      <TagInputContainer>
+        {hashTags.map((tag, index) => (
+          <Tag key={index}>#{tag}</Tag>
+        ))}
+        <Tag>
+          #
+          <TagInput
+            type='text'
+            value={currentTag}
+            ref={tagInputRef}
+            placeholder={!hashTags.length ? placeholder : ''}
+            onChange={handleInputChange}
+            onKeyDown={handleInputKeyDown}
+          />
+        </Tag>
+      </TagInputContainer>
+      {currentTag && (
+        <div className='searchedTags'>
+          {searchResult.map((hashtag) => {
+            return (
+              <SearchedHashTag
+                key={hashtag.hashtagId}
+                onClick={() => onClickSearchedHashtag(hashtag.hashtagContent)}
+              >
+                <span>#</span>
+                <HighlightedText searchText={currentTag} text={hashtag.hashtagContent} />
+                <span>{hashtag.count}</span>
+              </SearchedHashTag>
+            )
+          })}
+        </div>
+      )}
+    </HashTagWrapper>
   )
 }
 
--- src/pages/item/addInfo/components/HashTags/styles.tsx
+++ src/pages/item/addInfo/components/HashTags/styles.tsx
@@ -1,23 +1,59 @@
 import styled from '@emotion/styled'
+import { Common, Pretendard } from '../../../../../components/styles'
 
-export const HashTagInput = styled.textarea`
+export const Container = styled.div`
+  display: flex;
+  white-space: nowrap;
+  align-items: center;
+  width: 100%;
+  ${Pretendard({ size: 16, weight: Common.bold.thin, color: Common.colors.BK })}
+`
+
+export const HashTagInput = styled.input`
   border: none;
   outline: none;
-  font-family: 'Pretendard';
-  font-size: 1rem;
   width: 100%;
   background-color: transparent;
-  word-wrap: break-word;
-  white-space: pre-wrap;
-  overflow-wrap: break-word;
-  word-break: break-all;
-  resize: none; /* disallow resizing */
-
-  height: auto;
   line-height: 1.5em;
   max-height: 4.5em;
 
   &::placeholder {
     color: #999;
   }
+`
+export const TagInputContainer = styled.div`
+  display: block;
+
+  border: none;
+  outline: none;
+  width: 100%;
+  background-color: transparent;
+  word-wrap: break-word;
+  white-space: wrap;
+  overflow-wrap: break-word;
+  word-break: break-all;
+  box-sizing: border-box;
+  line-height: 1.5em;
+  ${Pretendard({ size: 16, weight: Common.bold.thin, color: Common.colors.BK })}
+  &::placeholder {
+    color: #999;
+  }
+`
+
+export const Tag = styled.span`
+  margin-right: 0.25rem;
+`
+
+export const TagInput = styled.input<{ placeholder?: string; value?: string }>`
+  border: none;
+  outline: none;
+  white-space: wrap;
+  flex-grow: 1;
+  ${Pretendard({ size: 16, weight: Common.bold.thin, color: Common.colors.BK })}
+  width: ${(props) =>
+    props.placeholder === '' ? `calc(3ch + ${props.value?.length ?? 0}ch)` : 'auto'};
+`
+export const SearchedHashTag = styled.div`
+  display: flex;
+  flex-direction: row;
 `
