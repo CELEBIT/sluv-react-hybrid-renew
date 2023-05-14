0f2e01f197729933ed5cfedb43bda97d5b7fbd14
Boin Kim <charles1236@kau.kr>
2023-05-14 17:55:39 +0900

    [#81] feat : 아이템 장소 모달 API 연동 완료

---
+++ src/apis/place/hooks/usePostPlaceQuery.tsx
@@ -0,0 +1,28 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import PlaceService from '../placeService'
+import { queryKeys } from '../../../config/queryKeys'
+import useModals from '../../../components/Modals/hooks/useModals'
+import { modals } from '../../../components/Modals'
+
+interface IPostItemPlace {
+  placeName: string
+}
+
+const usePostPlaceQuery = () => {
+  const place = new PlaceService()
+  const queryClient = useQueryClient()
+  const { closeModal } = useModals()
+
+  const postItemPlace = useMutation(
+    ({ placeName }: IPostItemPlace) => place.postItemPlace(placeName),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(queryKeys.recentPlace)
+        closeModal(modals.ItemDatePickerModal)
+      },
+    },
+  )
+  return { postItemPlace }
+}
+
+export default usePostPlaceQuery
