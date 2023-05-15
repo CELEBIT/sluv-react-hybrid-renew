b381972bb8db7bfde3b62864bf95710113f4e873
ezenjun <ezenjun07@gmail.com>
2023-05-15 15:10:05 +0900

    [#92] feat : 정보확인 페이지 생성

---
+++ src/pages/item/confirm/styles.tsx
@@ -0,0 +1,56 @@
+import styled from '@emotion/styled'
+
+export const ConfirmContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  width: 100vw;
+  margin-left: calc(-50vw + 50%);
+`
+export const PhotosWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  overflow-x: scroll;
+  padding-top: 1rem;
+  gap: 0.75rem;
+
+  padding-left: 1.25rem;
+  padding-right: 1.25rem;
+  ::-webkit-scrollbar {
+    display: hidden;
+    height: 0;
+    width: 0;
+  }
+
+  .row {
+    display: flex;
+    flex-direction: row;
+    gap: 0.75rem;
+  }
+`
+export const ItemWrapper = styled.div`
+  display: flex;
+  /* border: 1px solid blue; */
+  flex-direction: column;
+  height: 100%;
+  gap: 2.5rem;
+  overflow-y: scroll;
+  padding-bottom: 2.5rem;
+  ::-webkit-scrollbar {
+    display: none;
+  }
+  .padding {
+    padding: 0 1.25rem;
+  }
+`
+
+export const ConfirmButtonWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+  background-color: white;
+  border: 1px solid red;
+  padding: 0.75rem 1.25rem;
+`
