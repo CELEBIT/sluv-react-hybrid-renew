743925fa68ed59e6ba2b4156c15526fda4b323f2
Boin Kim <charles1236@kau.kr>
2023-05-17 21:31:18 +0900

    [#88] fix : itemCategory -> itemInfo Atom State로 수정

--- src/pages/item/create/components/SelectCategory/SelectCategory.tsx
+++ src/pages/item/create/components/SelectCategory/SelectCategory.tsx
@@ -9,8 +9,9 @@ import {
   selectedParentCategoryState,
   selectedSubCategoryState,
 } from '../../../../../components/BottomSheetModal/ItemCategoryModal'
-import { useRecoilState } from 'recoil'
+import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil'
 import { ChipWrapper } from './styles'
+import { itemInfoState } from '../../../../../recoil/itemInfo'
 
 const SelectCategory = () => {
   const { openModal } = useModals()
@@ -20,11 +21,13 @@ const SelectCategory = () => {
     getItemCategory: { data },
   } = useItemCategoryQuery()
 
+  const itemInfo = useRecoilValue(itemInfoState)
+
   const [categoryDisplayList, setCategoryDisplayList] = useRecoilState(CategoryDisplayListState)
   const [selectedParentCategory, setSelectedParentCategory] = useRecoilState(
     selectedParentCategoryState,
   )
-  const [selectedSubCategory, setSelectedSubCategory] = useRecoilState(selectedSubCategoryState)
+  const setSelectedSubCategory = useSetRecoilState(selectedSubCategoryState)
 
   const onCategoryClick = (category: Category) => {
     openModal(modals.ItemCategoryModal)
@@ -46,34 +49,56 @@ const SelectCategory = () => {
     }
   }, [data, setCategoryDisplayList])
 
+  // useEffect(() => {
+  //   if (data) {
+  //     const updatedList = data.filter((category) => category.id !== selectedParentCategory.id)
+  //     if (selectedParentCategory.id !== 9) {
+  //       const newDisplayItem = {
+  //         id: selectedParentCategory.id,
+  //         name:
+  //           data?.find((category) => category.id === selectedParentCategory.id)?.name +
+  //           '>' +
+  //           selectedSubCategory.name,
+  //         subCategoryList: selectedParentCategory.subCategoryList,
+  //       }
+  //       setCategoryDisplayList([newDisplayItem, ...updatedList])
+  //     } else {
+  //       const newDisplayItem = {
+  //         id: selectedParentCategory.id,
+  //         name: selectedParentCategory.name,
+  //       }
+  //       setCategoryDisplayList([newDisplayItem, ...updatedList])
+  //       // setSelectedSubCategory(newDisplayItem)
+  //     }
+  //   }
+  // }, [selectedParentCategory, selectedSubCategory])
+
   useEffect(() => {
-    if (data) {
-      const updatedList = data.filter((category) => category.id !== selectedParentCategory.id)
-      if (selectedParentCategory.id !== 9) {
-        const newDisplayItem = {
-          id: selectedParentCategory.id,
-          name:
-            data?.find((category) => category.id === selectedParentCategory.id)?.name +
-            '>' +
-            selectedSubCategory.name,
-          subCategoryList: selectedParentCategory.subCategoryList,
-        }
-        setCategoryDisplayList([newDisplayItem, ...updatedList])
-      } else {
-        const newDisplayItem = {
-          id: selectedParentCategory.id,
-          name: selectedParentCategory.name,
-        }
-        setCategoryDisplayList([newDisplayItem, ...updatedList])
-        // setSelectedSubCategory(newDisplayItem)
-      }
+    if (!data) {
+      return
     }
-  }, [selectedParentCategory, selectedSubCategory])
+    const parentCategoryId = itemInfo.itemCategory?.parentCategoryId
+    const filteredListByParent = data.filter((category) => category.id !== parentCategoryId)
+    if (parentCategoryId !== 9) {
+      const newDisplayItem = {
+        id: parentCategoryId ?? -1,
+        name:
+          (itemInfo.itemCategory?.parentName ?? '오류') + '>' + itemInfo.itemCategory?.childName,
+        subCategoryList: selectedParentCategory.subCategoryList,
+      }
+      setCategoryDisplayList([newDisplayItem, ...filteredListByParent])
+    } else {
+      const newDisplayItem = {
+        id: itemInfo.itemCategory?.parentCategoryId ?? -1,
+        name: itemInfo.itemCategory?.parentName ?? '오류',
+      }
+      setCategoryDisplayList([newDisplayItem, ...filteredListByParent])
+    }
+  }, [itemInfo.itemCategory?.parentCategoryId, itemInfo.itemCategory?.categoryId])
 
   return (
-    // <SelectCategoryWrapper>
     <ChipWrapper ref={selectRef}>
-      {selectedParentCategory.id !== 0 && selectedSubCategory.id !== 0 ? (
+      {itemInfo.itemCategory?.parentCategoryId && itemInfo.itemCategory?.categoryId ? (
         <>
           {categoryDisplayList?.map((category) => {
             return (
@@ -82,7 +107,10 @@ const SelectCategory = () => {
                 text={category.name}
                 icon={true}
                 type='pri'
-                active={selectedParentCategory.id === category.id && selectedSubCategory.id !== 0}
+                active={
+                  itemInfo.itemCategory?.parentCategoryId === category.id &&
+                  itemInfo.itemCategory?.categoryId !== 0
+                }
                 onClick={() => onCategoryClick(category)}
               ></ButtonMedium>
             )
@@ -90,7 +118,7 @@ const SelectCategory = () => {
         </>
       ) : (
         <>
-          {selectedParentCategory.id === 9 ? (
+          {itemInfo.itemCategory?.parentCategoryId === 9 ? (
             <>
               {categoryDisplayList?.map((category) => {
                 return (
@@ -99,7 +127,7 @@ const SelectCategory = () => {
                     text={category.name}
                     icon={true}
                     type='pri'
-                    active={selectedParentCategory.id === category.id}
+                    active={itemInfo.itemCategory?.parentCategoryId === category.id}
                     onClick={() => onCategoryClick(category)}
                   ></ButtonMedium>
                 )
@@ -116,10 +144,10 @@ const SelectCategory = () => {
                       icon={true}
                       type='pri'
                       active={
-                        selectedParentCategory.id === 9
+                        itemInfo.itemCategory?.parentCategoryId === 9
                           ? true
-                          : selectedParentCategory.id === category.id &&
-                            selectedSubCategory.id !== 0
+                          : itemInfo.itemCategory?.parentCategoryId === category.id &&
+                            itemInfo.itemCategory?.categoryId !== 0
                       }
                       onClick={() => onCategoryClick(category)}
                     ></ButtonMedium>
@@ -130,7 +158,6 @@ const SelectCategory = () => {
         </>
       )}
     </ChipWrapper>
-    // </SelectCategoryWrapper>
   )
 }
 
