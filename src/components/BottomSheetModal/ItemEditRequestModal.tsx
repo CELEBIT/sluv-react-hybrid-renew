dc0ee81877857487a53d4cb89b8b87c84d586ec1
ezenjun <ezenjun07@gmail.com>
2023-07-11 23:43:34 +0900

    [#137] design : 공유하기 icon 색상 수정 및 게시글 상세 더보기 모달 수정

--- src/components/BottomSheetModal/ItemEditRequestModal.tsx
+++ src/components/BottomSheetModal/ItemEditRequestModal.tsx
@@ -10,6 +10,7 @@ import { modals } from '../Modals'
 import { Common, Pretendard } from '../styles'
 import { useNavigate } from 'react-router-dom'
 import { RequestEditItemState } from '../../pages/item/editRequest'
+import { ReactComponent as Share } from '../../assets/share_24.svg'
 
 const ItemEditRequestModal = () => {
   const navigate = useNavigate()
@@ -38,9 +39,14 @@ const ItemEditRequestModal = () => {
           modalCloseBtnClick={() => closeModal(modals.ItemEditRequestModal)}
         />
         <MenuWrapper>
-          <Menu onClick={onClickEditRequest}>정보 수정 요청하기</Menu>
-          <Menu onClick={onClickReportItem}>게시글 신고하기</Menu>
+          <Menu onClick={onClickEditRequest}>
+            <Share stroke={Common.colors.BK}></Share>정보 수정 요청하기
+          </Menu>
+          <Menu onClick={onClickReportItem}>
+            <Share stroke={Common.colors.BK}></Share>게시글 신고하기
+          </Menu>
           <Menu onClick={onClickReportUser}>
+            <Share stroke={Common.colors.BK}></Share>
             &apos;{EditReportItem.itemWriterName}&apos;님 신고하기
           </Menu>
         </MenuWrapper>
@@ -54,19 +60,20 @@ const ModalWrapper = styled.div`
   flex-direction: column;
   width: 100%;
   height: 100%;
-  padding: 0 1.25rem 0.75rem 20px;
+  padding: 0 1.25rem 0.75rem 2rem;
 `
 const MenuWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
   width: 100%;
   height: 100%;
-  gap: 1.5rem;
-  padding-bottom: 1rem;
 `
 
-const Menu = styled.span`
+const Menu = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 0.875rem 0;
+  gap: 0.625rem;
   ${Pretendard({ size: 17, weight: Common.bold.regular, color: Common.colors.BK })}
 `
 
