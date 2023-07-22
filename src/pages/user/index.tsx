0886fd84e3b151d0c66b20469d404ea5b1258112
ezenjun <ezenjun07@gmail.com>
2023-07-22 22:56:50 +0900

    [#138] feat : 관심셀럽 사이드바 개발 중

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const User = () => {
-  return <div>User</div>
+  const navigate = useNavigate()
+  return (
+    <div>
+      User
+      <div
+        onClick={() => {
+          navigate('/select-celeb')
+        }}
+      >
+        관심셀럽 선택하기
+      </div>
+    </div>
+  )
 }
 
 export default User
