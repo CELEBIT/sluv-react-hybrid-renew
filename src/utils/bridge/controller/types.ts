24bfca2c4972aa63341ec3f06dbf911dec66665e
Leein970 <in.lee@spendit.kr>
2023-08-16 22:02:03 +0900

    chore : 서비스 모듈 내보내기 선언

---
+++ src/utils/bridge/controller/types.ts
@@ -0,0 +1,32 @@
+import {
+  BridgeMessage,
+  BridgeMessageHeader,
+  BridgeMessagePayload,
+  BridgeMessageType,
+  BridgeService,
+  BridgeServiceManager,
+} from '../service'
+import { generateRandomHash } from '../../hash'
+import { ServiceList } from '../service/list'
+
+export type BridgeServices = typeof ServiceList
+export type BridgeServiceKeys = keyof BridgeServices
+
+export type BridgeServiceObserver<
+  TReqPayload = unknown,
+  TResPayload = unknown,
+  S extends BridgeService<TReqPayload, TResPayload> = BridgeService<TReqPayload, TResPayload>,
+> = (this: S, message?: BridgeMessage<TReqPayload | TResPayload>) => void
+
+export type RequestPayloadOf<S extends keyof BridgeServices> = Parameters<
+  BridgeServices[S]['requestMessage']
+>[1]
+export type ReceivePayloadOf<S extends keyof BridgeServices> = Parameters<
+  BridgeServices[S]['receiveMessage']
+>[1]
+
+export interface MappedObserver<S extends BridgeServiceKeys> {
+  code: string
+  observer: BridgeServiceObserver<RequestPayloadOf<S>, ReceivePayloadOf<S>>
+  service: BridgeServices[S]
+}
