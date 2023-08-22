f6390141282583986b2b13b1357f589a7ab14c38
Leein970 <in.lee@spendit.kr>
2023-08-22 18:35:30 +0900

    feat : 해쉬 생성 함수 crypto 종속성 제거

--- src/utils/hash/index.ts
+++ src/utils/hash/index.ts
@@ -1,5 +1,17 @@
-import crypto from 'crypto'
+const getRandomChar = (): string => {
+  const charType = Math.floor(Math.random() * 3)
+  switch (charType) {
+    case 0: // 대문자
+      return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+    case 1: // 소문자
+      return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+    case 2: // 숫자
+      return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
+    default:
+      return ''
+  }
+}
 
 export const generateRandomHash = (): string => {
-  return crypto.randomBytes(32).toString('hex')
+  return Array.from({ length: 64 }, getRandomChar).join('')
 }
