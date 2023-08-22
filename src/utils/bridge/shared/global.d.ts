b84c154577ccb5138f918bf07bd3881173926efd
Leein970 <in.lee@spendit.kr>
2023-08-22 18:39:16 +0900

    chore : 통신 메서드 타입 정의 변경

--- src/utils/bridge/shared/global.d.ts
+++ src/utils/bridge/shared/global.d.ts
@@ -4,15 +4,14 @@ declare global {
   interface Window {
     AndroidBridge: {
       sendToNative: (message: string) => void
-      sendToWebview: (message: BridgeMessage<any>) => void
     }
     webkit: {
       messageHandlers: {
         IOSBridge: {
           sendToNative: (message: string) => void
-          sendToWebview: (message: BridgeMessage<any>) => void
         }
       }
     }
+    sendToWebview: (message: string) => void
   }
 }
