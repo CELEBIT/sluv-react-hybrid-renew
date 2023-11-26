56ade4122be6c0897307a02d684c5e4d1ae3cc46
ezenjun <ezenjun07@gmail.com>
2023-11-26 14:49:33 +0900

    [#151] design : 커뮤니티 홈 질문하기 버튼 수정

--- src/pages/community/components/WriteCommunityItemButton/styles.tsx
+++ src/pages/community/components/WriteCommunityItemButton/styles.tsx
@@ -1,4 +1,9 @@
 import styled from '@emotion/styled'
+import { Common, Pretendard } from '../../../../components/styles'
+
+interface WriteQuestionButtonProps {
+  hasButtonText?: boolean
+}
 
 export const WriteCommunityItemButtonWrapper = styled.div`
   display: flex;
@@ -6,4 +11,20 @@ export const WriteCommunityItemButtonWrapper = styled.div`
   position: fixed;
   bottom: 4.375rem;
   right: 1.25rem;
+`
+
+export const WriteQuestionButton = styled.div<WriteQuestionButtonProps>`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  gap: 0.375rem;
+  background-color: ${Common.colors.BK};
+  border-radius: 6.25rem;
+  padding: ${(props) => (props.hasButtonText ? '0.75rem 1.25rem' : '0.75rem')};
+  transition: ease-in-out 5ms;
+`
+
+export const ButtonText = styled.span`
+  ${Pretendard({ size: 15, weight: Common.bold.regular, color: Common.colors.WH })}
 `
