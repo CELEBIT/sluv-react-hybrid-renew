da516a4366944daff322445ee563955824dd65c3
ezenjun <ezenjun07@gmail.com>
2024-02-06 17:09:32 +0900

    [#170] modify : 홈 유저 팔로우/팔로잉 수정

--- src/pages/home/components/WeeklyTopUser/UserCard/UserCard.tsx
+++ src/pages/home/components/WeeklyTopUser/UserCard/UserCard.tsx
@@ -4,24 +4,26 @@ import { ReactComponent as RankFirst } from '../../../../../assets/rank_1.svg'
 import { ReactComponent as RankSecond } from '../../../../../assets/rank_2.svg'
 import { ReactComponent as RankThird } from '../../../../../assets/rank_3.svg'
 import FollowMediumButton from '../../../../../components/ButtonMedium/FollowMediumButton'
+import useFollowQuery from '../../../../../apis/user/hooks/useFollowQuery'
+import { IUserResult } from '../../../../../apis/user/userService'
 
 interface UserCardProps {
   rank?: number
   imgUrl: string
-  userName: string
+  user: IUserResult
   followStatus?: boolean
   borderRadius?: number
   onClick: () => void
 }
 
-const UserCard = ({
-  rank,
-  imgUrl,
-  followStatus,
-  userName,
-  borderRadius,
-  onClick,
-}: UserCardProps) => {
+const UserCard = ({ rank, imgUrl, followStatus, user, borderRadius, onClick }: UserCardProps) => {
+  const {
+    followUser: { mutate: mutateByFollow },
+  } = useFollowQuery()
+  const onClickFollow = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>, userId: number) => {
+    e.stopPropagation() // Stop propagation here
+    mutateByFollow({ userId: userId })
+  }
   return (
     <UserCardWrapper borderRadius={borderRadius} onClick={onClick}>
       {rank && rank <= 3 && (
@@ -34,13 +36,13 @@ const UserCard = ({
         </div>
       )}
       <UserPhoto imgUrl={imgUrl}></UserPhoto>
-      <UserNickName>{userName}</UserNickName>
+      <UserNickName>{user?.nickName}</UserNickName>
       {followStatus ? (
-        <FollowMediumButton icon={true} active={false} onClick={() => console.log('clicked')}>
+        <FollowMediumButton icon={true} active={false} onClick={(e) => onClickFollow(e, user.id)}>
           팔로잉
         </FollowMediumButton>
       ) : (
-        <FollowMediumButton icon={false} active={true} onClick={() => console.log('clicked')}>
+        <FollowMediumButton icon={false} active={true} onClick={(e) => onClickFollow(e, user.id)}>
           팔로우
         </FollowMediumButton>
       )}
