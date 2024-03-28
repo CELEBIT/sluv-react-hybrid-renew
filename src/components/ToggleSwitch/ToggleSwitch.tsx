167ebeb861e8b91585b4c1ee5cd39f3deaccd24c
ezenjun <ezenjun07@gmail.com>
2024-03-28 19:30:52 +0900

    [#170] feat : 마케팅 정보 수신 동의 토글 생성

--- src/components/ToggleSwitch/ToggleSwitch.tsx
+++ src/components/ToggleSwitch/ToggleSwitch.tsx
@@ -4,11 +4,19 @@ import { StyledLabel } from './styles'
 export type ToggleSwitchProps = React.HTMLAttributes<HTMLInputElement> & {
   onToggleSwitch?: (e: ChangeEvent<HTMLInputElement>) => void
   isToggleOn: boolean
+  onClick?: (e: React.MouseEvent<HTMLLabelElement>) => void
 }
 
-const ToggleSwitch = ({ isToggleOn, onToggleSwitch }: ToggleSwitchProps) => {
+const ToggleSwitch = ({ isToggleOn, onToggleSwitch, onClick }: ToggleSwitchProps) => {
+  const handleClick = (e: React.MouseEvent<HTMLLabelElement>) => {
+    if (onClick) {
+      onClick(e) // Execute onClick prop if provided
+    }
+    // Stop propagation to prevent the event from bubbling further
+    e.stopPropagation()
+  }
   return (
-    <StyledLabel htmlFor='checkbox' isToggleOn={isToggleOn}>
+    <StyledLabel htmlFor='checkbox' isToggleOn={isToggleOn} onClick={handleClick}>
       <input id='checkbox' type='checkbox' checked={isToggleOn} onChange={onToggleSwitch} />
     </StyledLabel>
   )
