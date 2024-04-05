248503519098420c164a94978b39ec124bb088f3
ezenjun <ezenjun07@gmail.com>
2024-04-05 21:27:31 +0900

    [#170] feat : 탈퇴하기 약관 안내 페이지 추가

--- src/pages/settings/RequestWithdraw/styles.tsx
+++ src/pages/settings/RequestWithdraw/styles.tsx
@@ -7,10 +7,53 @@ export const EditReportContainer = styled.div`
   width: 100%;
   height: 100%;
 `
+export const ConfirmWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  width: 100%;
+`
+export const ConfirmTitle = styled.span`
+  padding: 1.5rem 1.25rem;
+  ${Pretendard({ size: 24, weight: Common.bold.regular, color: Common.colors.BK })}
+`
+export const AgreementBg = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100%;
+  width: 100%;
+  padding: 1.5rem 1.25rem 0.75rem 1.25rem;
+  background-color: ${Common.colors.GR50};
+  gap: 1.5rem;
+`
+
+export const Agreement = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+`
+
+export const EachContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.75rem;
+`
+
+export const AgreeTitle = styled.span`
+  ${Pretendard({ size: 15, weight: Common.bold.regular, color: Common.colors.BK })}
+`
+export const AgreeDetail = styled.span`
+  ${Pretendard({ size: 15, weight: Common.bold.regular, color: Common.colors.GR600 })}
+  line-height: 23px;
+`
+
 export const ReasonWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  padding: 1.5rem 0;
+  height: 100%;
+  width: 100%;
+  padding: 1.5rem 1.25rem;
 `
 export const EditReportListWrapper = styled.div`
   display: flex;
